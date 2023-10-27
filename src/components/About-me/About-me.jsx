2b0455fa10cbb5e6a9d1439a1990b837e459cac4
JMADA257 <jadams1812@hotmail.com>
2023-10-26 22:52:36 -0600

    Added descriptions to all projects

--- src/components/About-me/About-me.jsx
+++ src/components/About-me/About-me.jsx
@@ -17,16 +17,19 @@ function AboutMe() {
           <h1 className="aboutH1">Jordan Adams</h1>
           <h2 className="aboutH2">About me! </h2>
           <article className="aboutSection">
-            Welcome to my professional portfolio! I'm Jordan Adams, an aspiring
+            Welcome to my professional portfolio! I'm Jordan Adams, originally
+            from Delaware and currently residing in Denver. I'm an aspiring
             full-stack developer with a strong interest in three.js, 3D
-            modeling, and back-end development. With a three-year tenure as a
-            tank operator in the United States Marine Corps, I bring discipline
-            and determination to my work. Post-military service, I've gained
-            diverse experience, including car mechanics, hospitality service,
-            and, most recently, leading the bar at a multi-million-dollar
-            restaurant. I'm excited to connect with you. Please explore my
-            website, and feel free to reach out if you'd like to discuss
-            potential opportunities. Thank you for visiting!
+            modeling, back-end development, and a passion for space exploration.
+            With a three-year tenure as a tank operator in the United States
+            Marine Corps, I bring discipline and determination to my work.
+            Post-military service, I've gained diverse experience, including car
+            mechanics, hospitality service, and, most recently, leading the bar
+            at a multi-million-dollar restaurant. Below, you'll find a 3D model
+            of the Earth with a red dot marking my current location in Denver.
+            I'm excited to connect with you. Please explore my website, and feel
+            free to reach out if you'd like to discuss potential opportunities.
+            Thank you for visiting!
           </article>
         </section>
       </div>
--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -17,61 +17,106 @@ function CustomCarousel() {
         indicators={false}
         fade={true}
         touch={true}
-        interval={3500}
+        interval={9000}
       >
         <Carousel.Item>
           <img src={ShredNStyle} alt="First slide" className="img-fluid" />
-          <h2 className="projectDescription">Hello world Im project one!</h2>
-          <p>
-            {" "}
-            className="projectDescription" Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Asperiores consectetur culpa quos
-            recusandae ratione ea totam. Minus quae ipsum.
+          <h2 className="projectDescription">SkiNStyle</h2>
+          <p className="projectDescriptionPara">
+            Our recent group project was the development of a ski and
+            snowboarding website. Our primary goal was to enable users to easily
+            check weather conditions at various resorts. By selecting a state,
+            users can view all the resorts located in that state. We've also
+            included a shopping list feature for ski and snowboard equipment and
+            apparel. Additionally, users can create accounts, receiving a
+            welcome email upon sign-up, and place orders. Throughout this
+            project, we gained valuable experience in both front-end and
+            back-end development, including working with two dedicated
+            databases. Feel free to explore the website and see our work in
+            action!
           </p>
         </Carousel.Item>
         <Carousel.Item>
           <img src={MenuMatch} alt="Second slide" className="img-fluid" />
-          <h2 className="projectDescription">Hello world Im project two!</h2>
+          <h2 className="projectDescription">Menu Match</h2>
           <p className="projectDescription">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-            consectetur culpa quos recusandae ratione ea totam. Minus quae
-            ipsum.
+            For my initial group project during my course, we developed a
+            front-end app aimed at enhancing the dining experience at dinner
+            parties. The app provides users with randomly generated drink and
+            food recommendations on the home screen. Users can also search for
+            drinks and food by ingredient or category. Importantly, when you
+            search for a cocktail or food item, the app displays the entire
+            recipe with detailed instructions on how to prepare it.
+            Additionally, the app features a shopping list functionality,
+            allowing users to add ingredients to their list when selecting a
+            cocktail or food item. This project was instrumental in deepening
+            our understanding of API integration and the proper methods for
+            implementing them effectively.
           </p>
         </Carousel.Item>
         <Carousel.Item>
           <img src={TechBlog} alt="Third slide" className="img-fluid" />
-          <h2 className="projectDescription">Hello world Im project three!</h2>
+          <h2 className="projectDescription">Tech Blog </h2>
           <p className="projectDescription">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-            consectetur culpa quos recusandae ratione ea totam. Minus quae
-            ipsum.
+            This project represents a significant milestone in my development
+            journey, as I designed and created it independently. The tech blog I
+            developed provides users with the capability to sign in, express
+            their thoughts, and share them on a dedicated forum. Through this
+            experience, I delved deeper into the mechanics of user
+            authentication and login systems, refining my skills in this
+            critical aspect of web development. Notably, this solo endeavor not
+            only enhanced my technical prowess but also bolstered my confidence
+            as a developer. It underscored my ability to take on complex tasks
+            and complete them successfully. The project's success served as a
+            testament to my growth and proficiency as a solo developer, a
+            milestone that I am proud to share with others.
           </p>
         </Carousel.Item>
         <Carousel.Item>
           <img src={JSQuiz} alt="Fourth slide" className="img-fluid" />
-          <h2 className="projectDescription">Hello world Im project four!</h2>
+          <h2 className="projectDescription">JSQuiz</h2>
           <p className="projectDescription">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-            consectetur culpa quos recusandae ratione ea totam. Minus quae
-            ipsum.
+            This personal project, constructed entirely by me from the ground
+            up, was a valuable learning experience. It taught me how to set
+            timers and deliver quizzes to users, focusing predominantly on
+            JavaScript logic. To add a touch of humor and camaraderie, I
+            included a few joke questions for the classmates I journeyed through
+            school with. While it posed several challenges, I am proud to say
+            that I persevered and successfully completed this project, gaining
+            important skills in the process.
           </p>
         </Carousel.Item>
         <Carousel.Item>
           <img src={WeatherDashboard} alt="Fifth slide" className="img-fluid" />
-          <h2 className="projectDescription">Hello world Im project five!</h2>
+          <h2 className="projectDescription">Weather Dashboard</h2>
           <p className="projectDescription">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-            consectetur culpa quos recusandae ratione ea totam. Minus quae
-            ipsum.
+            This project marked my initial collaboration with a team from Menu
+            Match, serving as an invaluable learning experience for navigating
+            GitHub merge conflict errors. It deepened my understanding of how
+            GitHub functions in a group setting and the strategic choices one
+            can make to prevent conflicts from arising. Additionally, this
+            endeavor broadened my knowledge of API calls and significantly
+            enhanced my proficiency in working with local storage. The project
+            itself is a fully functional weather app that simplifies the process
+            of checking the weather. Users need only input a city name, and the
+            app displays the current day's weather as well as forecasts for the
+            next seven days. It also efficiently stores these city references in
+            local storage, allowing for easy access to past searches.
           </p>
         </Carousel.Item>
         <Carousel.Item>
           <img src={PasswordGen} alt="Sixth slide" className="img-fluid" />
-          <h2 className="projectDescription">Hello world Im project six!</h2>
+          <h2 className="projectDescription">Password Generator</h2>
           <p className="projectDescription">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-            consectetur culpa quos recusandae ratione ea totam. Minus quae
-            ipsum.
+            This private project was developed to empower users in creating
+            customized passwords tailored to their preferences. By incorporating
+            user input, including factors like special characters and uppercase
+            letters, the password generator produces unique and secure
+            passwords. In the process of building this project, I gained
+            valuable insights into working with alerts and extracting user
+            information. This experience laid the foundation for my ongoing
+            development journey, equipping me with essential skills in user
+            interaction and data handling.
           </p>
         </Carousel.Item>
       </Carousel>
