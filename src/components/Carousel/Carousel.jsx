abbbed44e0943026151cf22e64a765463d616834
JMADA257 <jadams1812@hotmail.com>
2024-07-02 11:29:06 -0600

    Updating resume to include the most recent contract

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,7 +1,7 @@
 import Carousel from "react-bootstrap/Carousel";
 import "../Carousel/Carousel.css";
 import ShredNStyle from "../../assets/ShredNStyle.png";
-import MenuMatch from "../../assets/Menu-Match.png";
+import SuperPups from "../../assets/Super-Pups.png";
 import Blurb from "../../assets/Blurb.png";
 import Mars from "../Globe/Mars";
 import { useState } from "react";
@@ -23,6 +23,24 @@ function CustomCarousel() {
         interval={8500}
         onSlide={onSlideChange}
       >
+        <Carousel.Item>
+          <a href="http://www.superpupevents.com/" target="_blank">
+            <img src={SuperPups} alt="Second slide" className="img-fluid" />
+          </a>
+          <h2 className="projectDescription">Super Pup Events</h2>
+          <p className="projectDescriptionPara">
+            I successfully completed a contracted project for Super Pup Events,
+            delivering a fully deployed website integrated with a lightweight
+            Content Management System (CMS). Utilizing a full stack MERNG
+            (MongoDB, Express.js, React, Node.js, GraphQL) architecture, the
+            website features robust functionalities including user login and
+            authentication, seamless email integration capabilities, and is
+            hosted using MongoDB Atlas and Heroku. This project showcases my
+            proficiency in developing scalable web applications with modern
+            technologies and implementing essential user management and
+            communication features..
+          </p>
+        </Carousel.Item>
         <Carousel.Item>
           <a href="https://github.com/ljkahn/Blurb" target="_blank">
             <img src={Blurb} alt="Third slide" className="img-fluid" />
@@ -58,36 +76,8 @@ function CustomCarousel() {
             action!
           </p>
         </Carousel.Item>
-        <Carousel.Item>
-          <a href="https://github.com/jkellogg01/menu-match" target="_blank">
-            <img src={MenuMatch} alt="Second slide" className="img-fluid" />
-          </a>
-          <h2 className="projectDescription">Menu Match</h2>
-          <p className="projectDescriptionPara">
-            For my initial group project during my course, we developed a
-            front-end app aimed at enhancing the dining experience at dinner
-            parties. The app provides users with randomly generated drink and
-            food recommendations on the home screen. Users can also search for
-            drinks and food by ingredient or category. Importantly, when you
-            search for a cocktail or food item, the app displays the entire
-            recipe with detailed instructions on how to prepare it.
-            Additionally, the app features a shopping list functionality,
-            allowing users to add ingredients to their list when selecting a
-            cocktail or food item. This project was instrumental in deepening
-            our understanding of API integration and the proper methods for
-            implementing them effectively.
-          </p>
-        </Carousel.Item>
       </Carousel>
-      {/* {planetIndex === 0 && <Mars />} */}
-      {/* {planetIndex === 1 && <h2>Mercury</h2>} */}
-      {/* {planetIndex === 1 && <Mercury />} */}
-      {/* {planetIndex === 2 && <Earth />} */}
-      {/* {planetIndex === 2 && <h2>Earth</h2>} */}
-
       <Mars visible={planetIndex === 0} />
-      {/* <Earth visible={planetIndex === 1} />
-      <Mercury visible={planetIndex === 2} /> */}
     </>
   );
 }
